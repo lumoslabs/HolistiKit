3bbf3a75981a02468e8238664cab85f407d107dc
Jesse Farless <solidcell@gmail.com>
2017-01-07 13:02:34 +0100

    Use a storyboard for the view controller

--- FringesExample/DashboardViewControllingFactory.swift
+++ FringesExample/DashboardViewControllingFactory.swift
@@ -3,9 +3,20 @@ import UIKit
 class DashboardViewControllingFactory: DashboardViewControllingFactoryProtocol {
 
     func create(withPresenter presenter: DashboardPresenter) -> DashboardViewControlling {
-        let viewController = DashboardViewController()
-        viewController.presenter = presenter
+        let viewController = DashboardViewController.create(presenter: presenter)
         return viewController
+    }
+}
+
+extension DashboardViewController {
+
+    static let storyboardName = "DashboardViewController"
+
+    class func create(presenter: DashboardPresenter) -> DashboardViewController {
+        let vc = UIStoryboard(name: storyboardName, bundle: nil)
+            .instantiateInitialViewController() as! DashboardViewController
+        vc.presenter = presenter
+        return vc
     }
 }
 
