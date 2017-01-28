61f7d2275c36bcaea67bc6c20c5d84e4073415ab
Jesse Farless <solidcell@gmail.com>
2017-01-28 23:29:42 +0100

    Move date provider notifier into a separate file

--- FringesExampleTests/SpecDateProvider.swift
+++ FringesExampleTests/SpecDateProvider.swift
@@ -1,9 +1,9 @@
 import Foundation
 @testable import FringesExample
 
-class SpecDateProvider {
+class SpecDateProvider: DateProviding {
 
-    fileprivate lazy var currentDate: Date = {
+    private(set) lazy var date: Date = {
         let initialDateString = "2016-08-23 00:00:00 +0000"
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
@@ -12,37 +12,8 @@ class SpecDateProvider {
 
     func progress(seconds: Int) {
         for _ in 0..<seconds {
-            currentDate = currentDate.addingTimeInterval(1)
-            SpecDateProviderNotifier.post(date: currentDate)
+            date = date.addingTimeInterval(1)
+            SpecDateProviderNotifier.post(date: date)
         }
-    }
-}
-
-extension SpecDateProvider: DateProviding {
-
-    var date: Date {
-        return currentDate
-    }
-}
-
-class SpecDateProviderNotifier {
-
-    static private let notificationCenter = NotificationCenter.default
-    static private let notificationName = Notification.Name("SpecDateProviderDateChanged")
-    static private let userInfoKey = "Date"
-
-    class func post(date: Date) {
-        notificationCenter.post(name: notificationName, object: nil, userInfo: [userInfoKey : date])
-    }
-
-    class func observe(_ block: @escaping (Date) -> Void) -> NSObjectProtocol {
-        return notificationCenter.addObserver(forName: notificationName, object: nil, queue: OperationQueue.main) { notification in
-            let date = notification.userInfo![userInfoKey] as! Date
-            block(date)
-        }
-    }
-    
-    class func remove(observer: Any) {
-        notificationCenter.removeObserver(observer, name: notificationName, object: nil)
     }
 }
