f1d12bcc24908e240eb3187660cc9a2bb551a1c9
Jesse Farless <solidcell@gmail.com>
2017-02-18 12:12:13 +0100

    Document system dialog text in comments

--- Frameworks/SpecUIKitFringes/SpecUIKitFringes/SystemDialog.swift
+++ Frameworks/SpecUIKitFringes/SpecUIKitFringes/SystemDialog.swift
@@ -5,13 +5,20 @@ public class SystemDialog {
     public init() { }
 
     public enum LocationManagerIdentifier {
-        // "Allow "TransitApp" to access your location while you use the app?"
+        // "Allow "<AppName>" to access your location while you use the app?"
         // "<message you have set in Info.plist for NSLocationWhenInUseUsageDescription>"
         // [Don't Allow] [Allow]
         case requestAccessWhileInUse
-        // Similar to the one above, but with different messaging.
+        // When status is notDetermined:
+        // "Allow "<AppName>" to access your location even when you are not using the app?"
+        // "<message you have set in Info.plist for NSLocationAlwaysUsageDescription>"
+        // [Don't Allow] [Allow]
+        // When status is already authorizedWhenInUse:
+        // "Allow "<AppName>" to also access your location even when you are not using the app?"
+        // "<message you have set in Info.plist for NSLocationAlwaysUsageDescription>"
+        // [Cancel] [Allow]
         case requestAccessAlways
-        // "Turn On Location Services to Allow "TransitApp" to Determine Your Location"
+        // "Turn On Location Services to Allow "<AppName>" to Determine Your Location"
         // [Settings] [Cancel]
         case requestJumpToLocationServicesSettings
     }
