e6c61fd57545ad7bb3e534ee2f943bd9c2c4d5b8
Jinwoo Baek <baek32jinoo@gmail.com>
2018-10-16 16:20:36 -0700

    Add another method to UserDefaulting

--- Frameworks/UIKitFringes/UIKitFringes/UserDefaults.swift
+++ Frameworks/UIKitFringes/UIKitFringes/UserDefaults.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public protocol UserDefaulting {
-    
     func set(_ value: Any?, forKey defaultName: String)
     func object(forKey defaultName: String) -> Any?
     func integer(forKey defaultName: String) -> Int
     func bool(forKey defaultName: String) -> Bool
+    func removeObject(forKey defaultName: String)
 }
 
 extension UserDefaults: UserDefaulting { }
