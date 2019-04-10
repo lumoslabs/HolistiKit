3fca3ca8070c3a3e32778cf67b38ebf712791170
Jinwoo Baek <baek32jinoo@gmail.com>
2019-04-10 16:00:39 -0700

    Add notification centering

--- Frameworks/SpecUIKitFringes/SpecUIKitFringes/NotificationCentering.swift
+++ Frameworks/SpecUIKitFringes/SpecUIKitFringes/NotificationCentering.swift
@@ -1,8 +1,15 @@
 import Foundation
 
 protocol NotificationCentering {
-    
     func post(name: NSNotification.Name, object: Any?)
+    func removeObserver(_ observer: Any)
+    func removeObserver(_ observer: Any, name aName: NSNotification.Name?, object anObject: Any?)
+    func addObserver(_ observer: Any, selector aSelector: Selector, name aName: NSNotification.Name?, object anObject: Any?)
+    func addObserver(forName name: NSNotification.Name?, object obj: Any?, queue: OperationQueueProtocol?, using block: @escaping (Notification) -> Void) -> NSObjectProtocol
 }
 
-extension NotificationCenter: NotificationCentering {}
+extension NotificationCenter: NotificationCentering {
+    public func addObserver(forName name: NSNotification.Name?, object obj: Any?, queue: OperationQueueProtocol?, using block: @escaping (Notification) -> Void) -> NSObjectProtocol {
+        return addObserver(forName: name, object: obj, queue: queue?.realQueue(), using: block)
+    }
+}
